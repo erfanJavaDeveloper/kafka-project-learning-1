f83ea0db9a96bef6072d58f1f318704a7de09dfc
eadine <erfan.adine.payeshgaranmodiriat@gmil.com>
2022-12-20 15:07:47 +0330

    start project

---
+++ src/main/java/com/example/springbootkafkatest/SpringBootKafkaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootkafkatest/SpringBootKafkaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
