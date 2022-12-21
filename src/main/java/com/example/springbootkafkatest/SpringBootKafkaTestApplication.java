9b842259faf18d8f0fd09624abdde0299a664c67
eadine <erfan.adine.payeshgaranmodiriat@gmil.com>
2022-12-21 10:46:58 +0330

    add KafkaConsumerConfig

--- src/main/java/com/example/springbootkafkatest/SpringBootKafkaTestApplication.java
+++ src/main/java/com/example/springbootkafkatest/SpringBootKafkaTestApplication.java
@@ -1,7 +1,10 @@
 package com.example.springbootkafkatest;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class SpringBootKafkaTestApplication {
@@ -10,4 +13,10 @@ public class SpringBootKafkaTestApplication {
         SpringApplication.run(SpringBootKafkaTestApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(KafkaTemplate<String,String> kafkaTemplate){
+        return args -> {
+            kafkaTemplate.send("erfanCode","hello kafka");
+        };
+    }
 }
